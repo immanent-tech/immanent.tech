6f618abce1094fcc650bce87d3a9ff4b7547cc79
Joshua Rich <joshua.rich@gmail.com>
2025-12-09 04:17:40 +0000

    feat: :rocket: initial commit

---
+++ .devcontainer/postcreate.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+set -x
+
+# Add starship to fish shell.
+mkdir -p ~/.config/fish
+echo "starship init fish | source" >>~/.config/fish/config.fish
+
+# Add starship to bash shell.
+echo 'eval "$(starship init bash)"' >>~/.bashrc
+
+cd /workspace
+
+go mod tidy
+
+exit 0
